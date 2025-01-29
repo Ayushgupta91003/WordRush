20e11a8f61ed81bf67dc464617cc21dd525648bf
ayush <ayush91003@gmail.com>
2025-01-29 16:57:22 +0530

    server started and running successfully

--- server/index.js
+++ server/index.js
@@ -1,7 +1,37 @@
-// post
-// set
-// get
-// update
-// delete
-console.log("Hello World");
-// console.log("Hello World");
+// // post
+// // set
+// // get
+// // update
+// // delete
+// // console.log("Hello World");
+// // console.log("Hello World");
+// const name = "ayush";
+// console.log(name);
+
+
+// IMPORTS
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+
+// CREATING A SERVER
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+// middle ware
+app.use(express.json()); // whatever data is stored/ received is in the json format.
+
+// FRONTEND -> MIDDLEWARE -> BACKEND. MIDDLEWARE IS USED FOR MANIPULATING THE DATA
+
+// CONNECT TO MONGODB
+
+
+// LISTEN TO SERVER
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server started and running on port ${port}`);
+})
+
+
+// EVERYTING IN JS IS AN OBJECT
+
